2a8d8c9e7b1da125d0307a367503663272521e35
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-03-27 23:49:51 +0200

    Working peterson algo

--- assignments/assignment2/Ex1Case1.java
+++ assignments/assignment2/Ex1Case1.java
@@ -1,5 +1,5 @@
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Ex1Case1 {
     private static volatile int sharedCounter = 0;
@@ -10,33 +10,55 @@ public class Ex1Case1 {
     private static int NUM_THREADS;
 
     private static PetersonLock lock;
-    private static final ReentrantLock rlock = new ReentrantLock();
 
     public static class PetersonLock {
         private int n;
-        private AtomicInteger[] level;
-        private AtomicInteger[] victim;
+        private AtomicIntegerArray level;
+        private AtomicIntegerArray victim;
 
 
         public PetersonLock(int n) {
             this.n = n;
-            this.level = new AtomicInteger[n];
-            this.victim = new AtomicInteger[n - 1];
+            this.level = new AtomicIntegerArray(n);
+            this.victim = new AtomicIntegerArray(n);
         }
 
         public void lock(int id) {
-            for (int i = 0; i < n; i++) {
-                level[id].set(i);
-                victim[i].set(id);
+            ArrayList<Integer> otherThreadIds = new ArrayList<Integer>();
 
-                while (victim[i].get() == id) {
+            for (int i = 0; i < n; i++) {
+                if (i == id) {
+                    continue;
+                }
+
+                otherThreadIds.add(i);
+            }
+
+            for (int l = 1; l < n; l++) {
+                level.set(id, l);
+                victim.set(l, id);
+
+                while (anotherThreadWaiting(otherThreadIds, l) && victim.get(l) == id) {
                     // busy wait
                 }
             }
         }
 
         public void unlock(int id) {
-//            level[id].set(0);
+            level.set(id, 0);
+        }
+
+        private boolean anotherThreadWaiting(ArrayList<Integer> threadIds, int currentLevel) {
+            boolean otherWaiting = false;
+
+            for (int threadId : threadIds) {
+                if (level.get(threadId) >= currentLevel) {
+                    otherWaiting = true;
+                    continue;
+                }
+            }
+
+            return otherWaiting;
         }
     }
 
@@ -63,8 +85,13 @@ public class Ex1Case1 {
 
 
     public static void main(String[] args) {
-        COUNTER_LIMIT = 1000;
-        NUM_THREADS = 4;
+        if (args.length != 2) {
+            System.out.println("Usage: java Ex1Case1 <NUM_THREADS> <COUNTER_LIMIT>");
+            return;
+        }
+
+        NUM_THREADS = Integer.parseInt(args[0]);
+        COUNTER_LIMIT = Integer.parseInt(args[1]);
 
         threads = new IncrementerThread[NUM_THREADS];
         accesses = new int[NUM_THREADS];
