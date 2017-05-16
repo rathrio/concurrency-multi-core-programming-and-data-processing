7d4a6f9610441fad613246ab7de3fbff25ccd0c0
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-05-16 23:48:47 +0200

    Add sequential smoothing algo

--- assignments/assignment5/Ex1.java
+++ assignments/assignment5/Ex1.java
@@ -7,12 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ex1 {
-    private static int[][] pixels;
-    private static int width;
-    private static int height;
+    private static int[][] PIXELS;
+    private static int WIDTH;
+    private static int HEIGHT;
 
     // If a pixel has >= D live neighbors, it will not be turned off.
     private static int D;
+
+    private static int ALIVENESS_THRESHOLD = 255;
 
     // Loads pgm image to the 2D int array pixels in this class.
     private static void loadImage(String path) {
@@ -32,18 +34,18 @@ public class Ex1 {
 
             String[] dimensionsArray = dimensions.split("\\s");
 
-            width = Integer.parseInt(dimensionsArray[0]);
-            height = Integer.parseInt(dimensionsArray[1]);
+            WIDTH = Integer.parseInt(dimensionsArray[0]);
+            HEIGHT = Integer.parseInt(dimensionsArray[1]);
 
-            pixels = new int[width][height];
+            PIXELS = new int[WIDTH][HEIGHT];
 
             int x = 0;
             int y = 0;
 
             for (String line : lines) {
-                pixels[x][y] = Integer.parseInt(line);
+                PIXELS[x][y] = Integer.parseInt(line);
 
-                x = (x + 1) % width;
+                x = (x + 1) % WIDTH;
                 if (x == 0) {
                     y++;
                 }
@@ -61,12 +63,12 @@ public class Ex1 {
 
         lines.add("P2");
         lines.add("# CREATOR: Radi");
-        lines.add(width + " " + height);
+        lines.add(WIDTH + " " + HEIGHT);
         lines.add("255");
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                lines.add(Integer.toString(pixels[x][y]));
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                lines.add(Integer.toString(PIXELS[x][y]));
             }
         }
 
@@ -78,60 +80,121 @@ public class Ex1 {
     }
 
     private static void sequentialSmoothEdges() {
-        int[][] tmpPixels = pixels.clone();
+        long startTime = System.nanoTime();
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int value = pixels[x][y];
+        int[][] tmpPixels = null;
 
-                // Skip not alive pixels.
-                if (value < 255) {
-                    continue;
-                }
+        while (tmpPixels != PIXELS) {
+            tmpPixels = PIXELS.clone();
 
-                // If current pixel doesn't have at least D alive neighbors,
-                // turn it off.
-                if (numAliveNeighbors(x, y) < D) {
-                    tmpPixels[x][y] = 255;
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
+                    int value = PIXELS[x][y];
+
+                    // Skip not alive pixels.
+                    if (value >= ALIVENESS_THRESHOLD) {
+                        continue;
+                    }
+
+                    // If current pixel doesn't have at least D alive neighbors,
+                    // turn it off.
+                    if (numAliveNeighbors(x, y) < D) {
+                        tmpPixels[x][y] = ALIVENESS_THRESHOLD;
+                    }
                 }
             }
+
+            PIXELS = tmpPixels;
         }
 
-        pixels = tmpPixels;
+        long runTime = System.nanoTime() - startTime;
+        double runTimeMs = runTime / 1000000.0;
+        System.out.println("Runtime: " + runTimeMs + "ms");
     }
 
     private static int numAliveNeighbors(int x, int y) {
-        ArrayList<Integer> neighbors = new ArrayList<Integer>();
+        ArrayList<Integer> neighbors = new ArrayList<>();
 
         if (x == 0 && y == 0) {
             // Top-left corner
-            neighbors.add(pixels[x + 1][y]);
-            neighbors.add(pixels[x + 1][y + 1]);
-            neighbors.add(pixels[x][y + 1]);
-        } else if ((x == (width - 1)) && (y == (height - 1))) {
+            neighbors.add(PIXELS[x + 1][y]);
+            neighbors.add(PIXELS[x + 1][y + 1]);
+            neighbors.add(PIXELS[x][y + 1]);
+        } else if ((x == (WIDTH - 1)) && (y == (HEIGHT - 1))) {
             // Bottom-right corner
-            neighbors.add(pixels[x - 1][y]);
-            neighbors.add(pixels[x - 1][y - 1]);
-            neighbors.add(pixels[x][y - 1]);
-        } else if ((x == (width - 1)) && (y == 0)) {
+            neighbors.add(PIXELS[x - 1][y]);
+            neighbors.add(PIXELS[x - 1][y - 1]);
+            neighbors.add(PIXELS[x][y - 1]);
+        } else if ((x == (WIDTH - 1)) && (y == 0)) {
             // Top-right corner
-            neighbors.add(pixels[x - 1][y]);
-            neighbors.add(pixels[x - 1][y + 1]);
-            neighbors.add(pixels[x][y + 1]);
-        } else if ((x == 0) && (y == (height - 1))) {
+            neighbors.add(PIXELS[x - 1][y]);
+            neighbors.add(PIXELS[x - 1][y + 1]);
+            neighbors.add(PIXELS[x][y + 1]);
+        } else if ((x == 0) && (y == (HEIGHT - 1))) {
             // Bottom-left corner
-            neighbors.add(pixels[x][y - 1]);
-            neighbors.add(pixels[x + 1][y - 1]);
-            neighbors.add(pixels[x + 1][y]);
+            neighbors.add(PIXELS[x][y - 1]);
+            neighbors.add(PIXELS[x + 1][y - 1]);
+            neighbors.add(PIXELS[x + 1][y]);
+        } else if (x == 0) {
+            // Left edge
+            neighbors.add(PIXELS[x][y - 1]);
+            neighbors.add(PIXELS[x + 1][y - 1]);
+            neighbors.add(PIXELS[x + 1][y]);
+            neighbors.add(PIXELS[x + 1][y + 1]);
+            neighbors.add(PIXELS[x][y + 1]);
+        } else if (x == (WIDTH - 1)) {
+            // Right edge
+            neighbors.add(PIXELS[x][y - 1]);
+            neighbors.add(PIXELS[x - 1][y - 1]);
+            neighbors.add(PIXELS[x - 1][y]);
+            neighbors.add(PIXELS[x - 1][y + 1]);
+            neighbors.add(PIXELS[x][y + 1]);
+        } else if (y == 0) {
+            // Top edge
+            neighbors.add(PIXELS[x - 1][y]);
+            neighbors.add(PIXELS[x - 1][y + 1]);
+            neighbors.add(PIXELS[x][y + 1]);
+            neighbors.add(PIXELS[x + 1][y + 1]);
+            neighbors.add(PIXELS[x + 1][y]);
+        } else if (y == (HEIGHT - 1)) {
+            // Bottom edge
+            neighbors.add(PIXELS[x - 1][y]);
+            neighbors.add(PIXELS[x - 1][y - 1]);
+            neighbors.add(PIXELS[x][y - 1]);
+            neighbors.add(PIXELS[x + 1][y - 1]);
+            neighbors.add(PIXELS[x + 1][y]);
+        } else {
+            // Somewhere in the middle
+            neighbors.add(PIXELS[x - 1][y]);
+            neighbors.add(PIXELS[x + 1][y]);
+            neighbors.add(PIXELS[x][y - 1]);
+            neighbors.add(PIXELS[x][y + 1]);
+            neighbors.add(PIXELS[x - 1][y - 1]);
+            neighbors.add(PIXELS[x + 1][y + 1]);
+            neighbors.add(PIXELS[x + 1][y - 1]);
+            neighbors.add(PIXELS[x - 1][y + 1]);
         }
 
+        // Count alive neighbors.
+        int count = 0;
+        for (int n : neighbors) {
+            if (n < ALIVENESS_THRESHOLD) {
+                count++;
+            }
+        }
 
-        return 0;
+        return count;
     }
 
     public static void main(String[] args) {
-        D = 3;
-        loadImage("images/lena.pgm");
+        if (args.length < 1) {
+            System.out.println("Usage: java Ex1 <D>");
+            return;
+        }
+
+        D = Integer.parseInt(args[0]);
+
+        loadImage("images/hasselhoff.pgm");
         sequentialSmoothEdges();
         writeImage("out.pgm");
     }
