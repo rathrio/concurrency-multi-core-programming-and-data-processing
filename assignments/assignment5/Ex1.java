616c49dfb768babd72d3a826e4ae645a9442d301
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-05-16 22:46:48 +0200

    Working pgm loader/writer

--- assignments/assignment5/Ex1.java
+++ assignments/assignment5/Ex1.java
@@ -1,6 +1,10 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ex1 {
@@ -8,6 +12,10 @@ public class Ex1 {
     private static int width;
     private static int height;
 
+    // If a pixel has >= d live neighbors, it will not be turned off.
+    private static int d;
+
+    // Loads pgm image to the 2D int array pixels in this class.
     private static void loadImage(String path) {
         try {
             List<String> lines = Files.readAllLines(Paths.get(path));
@@ -48,7 +56,34 @@ public class Ex1 {
         }
     }
 
+    private static void writeImage(String path) {
+        Path filePath = Paths.get(path);
+        ArrayList<String> lines = new ArrayList<>();
+
+        lines.add("P2");
+        lines.add("# CREATOR: Radi");
+        lines.add(width + " " + height);
+        lines.add("255");
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                lines.add(Integer.toString(pixels[x][y]));
+            }
+        }
+
+        try {
+            Files.write(filePath, lines, Charset.forName("ASCII"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void smoothEdges() {
+    }
+
     public static void main(String[] args) {
+        d = 3;
         loadImage("images/lena.pgm");
+        writeImage("out.pgm");
     }
 }
