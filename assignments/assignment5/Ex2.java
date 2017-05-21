2ffb27f3f795100f6d1968515fcf2f59891484a5
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-05-21 19:49:31 +0200

    Implement concurrent effects

--- assignments/assignment5/Ex2.java
+++ assignments/assignment5/Ex2.java
@@ -5,6 +5,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 public class Ex2 {
     private static int[][] PIXELS;
@@ -14,6 +16,46 @@ public class Ex2 {
     private static double[][] KERNEL = new double[3][3];
 
     private static int NUM_THREADS;
+
+    static class Worker extends Thread {
+        private int start, end;
+        private CyclicBarrier barrier;
+
+        public Worker(int start, int end, CyclicBarrier barrier) {
+            this.start = start;
+            this.end = end;
+            this.barrier = barrier;
+        }
+
+        @Override
+        public void run() {
+            int[][] tmpPixels = new int[WIDTH][HEIGHT];
+
+            // Apply the kernel on local strip.
+            for (int y = start; y < end; y++) {
+                for (int x = 1; x < (WIDTH - 1); x++) {
+                    double[][] neighborhood = neighborhoodMatrix(x, y, PIXELS);
+                    tmpPixels[x][y] = applyKernel(neighborhood);
+                }
+            }
+
+            // Wait for others to be done.
+            try {
+                barrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+
+            // Update global pixels with strip value.
+            for (int y = start; y < end; y++) {
+                for (int x = 1; x < (WIDTH - 1); x++) {
+                    PIXELS[x][y] = tmpPixels[x][y];
+                }
+            }
+        }
+    }
 
     // Loads pgm image to the 2D int array pixels in this class.
     private static void loadImage(String path) {
@@ -78,17 +120,21 @@ public class Ex2 {
         }
     }
 
+    // Sequential version that was used for comparison. Feel free to ignore this.
     private static void sequentialApplyEffect() {
+        System.out.println("Running sequentially...");
+
         long startTime = System.nanoTime();
 
         int[][] tmpPixels = new int[WIDTH][HEIGHT];
 
         for (int y = 1; y < (HEIGHT - 1); y++) {
             for (int x = 1; x < (WIDTH - 1); x++) {
-                double[][] neighborhood = neighborhoodMatrix(x, y);
+                double[][] neighborhood = neighborhoodMatrix(x, y, PIXELS);
                 tmpPixels[x][y] = applyKernel(neighborhood);
             }
         }
+
         PIXELS = tmpPixels;
 
         long runTime = System.nanoTime() - startTime;
@@ -96,8 +142,53 @@ public class Ex2 {
         System.out.println("Runtime: " + runTimeMs + "ms");
     }
 
+    // Since we're not dealing with "iterative" data parallelism, there's no
+    // need for a heartbeat approach. Threads do not depend on updated neighbor
+    // values, so they may apply the kernel on their strip, and simply wait for
+    // all other threads before updating the global pixels.
+    //
+    // The waiting is done with a CyclicBarrier.
     private static void parallelApplyEffect() {
+        System.out.println("Running concurrently...");
 
+        long startTime = System.nanoTime();
+
+        Worker[] workers = new Worker[NUM_THREADS];
+        CyclicBarrier barrier = new CyclicBarrier(NUM_THREADS);
+
+        int stripSize = (int) Math.ceil(HEIGHT / NUM_THREADS);
+
+        for (int i = 0; i < NUM_THREADS; i++) {
+            int start = i * stripSize;
+            int end = start + stripSize;
+
+            // Don't try to work on border strips. We'll ignore them, as described
+            // here: http://setosa.io/ev/image-kernels/.
+            if (start == 0) {
+                start = 1;
+            }
+
+            if (i == (NUM_THREADS - 1)) {
+                end = HEIGHT - 1;
+            }
+
+            Worker w = new Worker(start, end, barrier);
+            workers[i] = w;
+            w.start();
+        }
+
+        // Wait for all workers to update PIXELS.
+        for (int i = 0; i < NUM_THREADS; i++) {
+            try {
+                workers[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        long runTime = System.nanoTime() - startTime;
+        double runTimeMs = runTime / 1000000.0;
+        System.out.println("Runtime: " + runTimeMs + "ms");
     }
 
     private static int applyKernel(double[][] neighborhood) {
@@ -112,31 +203,32 @@ public class Ex2 {
         return (int) Math.round(value);
     }
 
-    private static double[][] neighborhoodMatrix(int x, int y) {
+    private static double[][] neighborhoodMatrix(int x, int y, int[][] matrix) {
         double[][] mat = new double[3][3];
-        mat[0] = new double[]{PIXELS[x - 1][y - 1], PIXELS[x - 1][y], PIXELS[x - 1][y + 1]};
-        mat[1] = new double[]{PIXELS[x][y - 1], PIXELS[x][y], PIXELS[x][y + 1]};
-        mat[2] = new double[]{PIXELS[x + 1][y - 1], PIXELS[x + 1][y], PIXELS[x + 1][y + 1]};
 
-//        for (int j = 0; j < 3; j++) {
-//            for (int i = 0; i < 3; i++) {
-//                System.out.print(PIXELS[j][i] + " ");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println();
+        mat[0] = new double[]{matrix[x - 1][y - 1], matrix[x - 1][y], matrix[x - 1][y + 1]};
+        mat[1] = new double[]{matrix[x][y - 1], matrix[x][y], matrix[x][y + 1]};
+        mat[2] = new double[]{matrix[x + 1][y - 1], matrix[x + 1][y], matrix[x + 1][y + 1]};
 
         return mat;
     }
 
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: java Ex1 <IMAGE> <EFFECT>");
+        if (args.length < 3) {
+            System.out.println("Usage: java Ex1 <IMAGE> <EFFECT> <NUM_THREADS> [<SEQUENTIAL>]");
             System.exit(1);
         }
 
         String path = args[0];
         String effect = args[1];
+        NUM_THREADS = Integer.parseInt(args[2]);
+
+        // Will use sequential method when set to 1.
+        int sequential = 0;
+
+        if (args.length > 3) {
+            sequential = Integer.parseInt(args[3]);
+        }
 
         switch (effect.toLowerCase()) {
             case "blur":
@@ -164,10 +256,14 @@ public class Ex2 {
                 System.exit(1);
         }
 
-        NUM_THREADS = 4;
-
         loadImage(path);
-        sequentialApplyEffect();
-        writeImage("out.pgm");
+
+        if (sequential != 0) {
+            sequentialApplyEffect();
+            writeImage("out_sequential.pgm");
+        } else {
+            parallelApplyEffect();
+            writeImage("out.pgm");
+        }
     }
 }
